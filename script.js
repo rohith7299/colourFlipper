3e4d8d14475547d93ffbef4405e9176ca64027d5
rohith <136072025+rohith7299@users.noreply.github.com>
2024-05-03 16:18:09 +0530

    hello

---
+++ script.js
@@ -0,0 +1,16 @@
+let btn =document.getElementById('btn');
+let span = document.getElementById('color');
+let hex = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+
+function changebg(){
+for(let i =0 ; i<=6 ; i++){
+    let cg =Math.floor()
+}}
+
+
+function random(){
+    let hex = Math.floor(Math.random()*16)
+    console.log(hex)
+    return hex
+}
+random()
